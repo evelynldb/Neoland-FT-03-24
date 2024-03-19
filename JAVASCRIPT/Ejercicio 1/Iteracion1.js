1b5cb268b0600223a4ecfdbde9b26107e0e5eef7
Evelyn <evelyndb@gmail.com>
2024-03-19 19:44:06 +0100

    Ejercicios 1

--- JAVASCRIPT/Ejercicio 1/Iteracion1.js
+++ JAVASCRIPT/Ejercicio 1/Iteracion1.js
@@ -1,8 +1,16 @@
+// // 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
 // 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
-// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
-// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
-// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
-// 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
-
 let myFavoriteHero = "Hulk";
-console.log("mi primer consolelog", myFavoriteHero);
+console.log("mi primer console log", myFavoriteHero);
+// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+let x = 50;
+console.log(x);
+
+// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+let h = 5;
+let y = 10;
+console.log(h, y);
+
+// 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+let z = h + y;
+console.log(z);
