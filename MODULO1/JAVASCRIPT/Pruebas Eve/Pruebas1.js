3804e96da72ddcb66e7dd8f58305a35de91901c1
Evelyn <evelyndb@gmail.com>
2024-04-13 18:16:54 +0200

    Modulo2_Preparación_Servidor

--- MODULO1/JAVASCRIPT/Pruebas Eve/Pruebas1.js
+++ MODULO1/JAVASCRIPT/Pruebas Eve/Pruebas1.js
@@ -1,20 +1,55 @@
-const addEventListeners = () => {
-  /** le damos el evento al boton de pokemon que es la unica pagina de contenido por
-   * ahora esta creada en el proyecto
-   */
-  const navigateHanged = document.getElementById("navigateHanged");
-  navigateHanged.addEventListener("click", () => {
-    initControler("Hanged");
-  });
-};
+/*Crea una variable llamada `myFavoriteHero`, asigna el valor `Hulk` a ella.*/
 
-export const printTemplateDashboard = () => {
-  /** Como siempre las paginas se renderizan en el main por lo cual inyectamos el template en el contenedor del main */
-  document.querySelector("main").innerHTML = template();
+const myFavoriteHero = "Hulk";
 
-  /** Para la nav, que la habiamos ocultado en el login, la volvemos a renderizar cambiandole el display de none a flex */
-  document.querySelector("nav").style.display = "flex";
+/*Crea una variable llamada `x`, asigna el valor `50` a ella.*/
 
-  /** metemos los escuchadores de la pagina */
-  addEventListeners();
-};
+const x = 50;
+
+/*Crea una variable llamada `h` con el valor `5` y otra `y` con el valor `10`. */
+
+const h = 5;
+const y = 10;
+
+/*Crea una otra variable `z` y asignale el valor de `h + y`.*/
+
+const z = h + y;
+
+console.log(z);
+
+/*Dado el siguiente javascript, cambia el valor de la propiedad `age` a `250`.*/
+
+const character = { name: "Jack Frost", age: 10 };
+
+character.age = 250;
+
+console.log(character);
+
+/*Declara 3 variables con los nombres y valores siguientes:*/
+
+/*firstName = "Jon";
+lastName = "Snow";
+age = 24;
+*/
+const firstName = "Jon";
+const lastName = "Snow";
+const age = 24;
+
+/*Guarda los valores en `**sentence**` con la siguiente estructura:*/
+
+const sentence = `Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.`;
+
+console.log(sentence);
+
+/*Dado el siguiente javascript, guarda en una variable la suma del precio de ambos juguetes.*/
+const toy1 = { name: "Buss myYear", price: 19 };
+const toy2 = { name: "Rallo mcKing", price: 29 };
+
+const totalToys = toy1.price + toy2.price;
+console.log(totalToys);
+
+const quote = "To infinity and beyond";
+
+const word = "infinity";
+
+console.log(quote.includes(word)); // Retorna true
